8678ad313b7a82e1f6c9727c5729b369c0f810f4
rail5 <andrew@rail5.org>
2023-03-26 17:52:47 -0400

    Cleaned up conditionals in insert() and remove()

--- edit/src/class_file.cpp
+++ edit/src/class_file.cpp
@@ -251,64 +251,66 @@ void editor::file::insert(long long int start_position, string text_to_insert) {
 		// Add a newline char
 		file_stream.seekp(new_file_length - 1, ios::beg);
 		file_stream.write("\n", 1);
-	} else {
-		// Writing BEFORE EOF
 		
-		int amount_to_store = block_size;
-		
-		// Adjust the length of the file by adding 0s to the end
-		ftruncate(file_descriptor, new_file_length);
-		
-		// Add a newline char
-		file_stream.seekp(new_file_length - 1, ios::beg);
-		file_stream.write("\n", 1);
-		
-		for (long long int i = (new_file_length - 1); i > start_position; i = (i - amount_to_store)) {
-		
-			long long int copy_to_this_position = (i - (amount_to_store - 1)) - 1;
-			long long int copy_from_this_position = (copy_to_this_position - insert_length);
+		return;
+	}
+	
+	// Writing BEFORE EOF
+	
+	int amount_to_store = block_size;
+	
+	// Adjust the length of the file by adding 0s to the end
+	ftruncate(file_descriptor, new_file_length);
+	
+	// Add a newline char
+	file_stream.seekp(new_file_length - 1, ios::beg);
+	file_stream.write("\n", 1);
+	
+	for (long long int i = (new_file_length - 1); i > start_position; i = (i - amount_to_store)) {
+	
+		long long int copy_to_this_position = (i - (amount_to_store - 1)) - 1;
+		long long int copy_from_this_position = (copy_to_this_position - insert_length);
 
-			// Final iteration:
-			// If we discover that our "copy_from" position is before our start_position,
-				// (Due to block_size shenanigans --
-				// E.g: block_size = 1024, but we only have 512 bytes left in the loop)
-			// Then we re-set the copy_from to the start_position,
-			// Fix the copy_to, change amount_to_store to what's LEFT
-				// (In the above example, 512 rather than 1024)
-			// And set i = start_position to make sure the loop doesn't run again after this
-			if (copy_from_this_position < start_position) {
+		// Final iteration:
+		// If we discover that our "copy_from" position is before our start_position,
+			// (Due to block_size shenanigans --
+			// E.g: block_size = 1024, but we only have 512 bytes left in the loop)
+		// Then we re-set the copy_from to the start_position,
+		// Fix the copy_to, change amount_to_store to what's LEFT
+			// (In the above example, 512 rather than 1024)
+		// And set i = start_position to make sure the loop doesn't run again after this
+		if (copy_from_this_position < start_position) {
+		
+			long long int difference = start_position - copy_from_this_position;
 			
-				long long int difference = start_position - copy_from_this_position;
-				
-				copy_from_this_position = start_position;
-				
-				copy_to_this_position = copy_to_this_position + difference;
-				
-				amount_to_store = amount_to_store - difference;
-				
-				i = start_position; // Terminate loop
-			}
+			copy_from_this_position = start_position;
 			
-			char* temp_data_storage = new char[amount_to_store + 1]{0}; // Allocate memory
+			copy_to_this_position = copy_to_this_position + difference;
 			
-			// Store read portion into allocated memory
-			file_stream.seekg(copy_from_this_position, ios::beg);
-			file_stream.read(temp_data_storage, amount_to_store);
+			amount_to_store = amount_to_store - difference;
 			
-			// Add a NUL char to the end to terminate the string
-			temp_data_storage[amount_to_store] = 0;
-			
-			// Copy it to its new proper place
-			file_stream.seekp(copy_to_this_position, ios::beg);
-			file_stream.write(temp_data_storage, amount_to_store);
-			
-			delete[] temp_data_storage; // Free memory
+			i = start_position; // Terminate loop
 		}
 		
-		// Now, finally, insert the damn data (user inputted data)
-		file_stream.seekp(start_position, ios::beg);
-		file_stream.write(text_to_insert.c_str(), text_to_insert.length());
+		char* temp_data_storage = new char[amount_to_store + 1]{0}; // Allocate memory
+		
+		// Store read portion into allocated memory
+		file_stream.seekg(copy_from_this_position, ios::beg);
+		file_stream.read(temp_data_storage, amount_to_store);
+		
+		// Add a NUL char to the end to terminate the string
+		temp_data_storage[amount_to_store] = 0;
+		
+		// Copy it to its new proper place
+		file_stream.seekp(copy_to_this_position, ios::beg);
+		file_stream.write(temp_data_storage, amount_to_store);
+		
+		delete[] temp_data_storage; // Free memory
 	}
+	
+	// Now, finally, insert the damn data (user inputted data)
+	file_stream.seekp(start_position, ios::beg);
+	file_stream.write(text_to_insert.c_str(), text_to_insert.length());
 }
 
 void editor::file::remove(long long int start_position, long long int end_position) {
@@ -331,51 +333,54 @@ void editor::file::remove(long long int start_position, long long int end_positi
 	
 	// Are we deleting the end of the file, or something before the end of the file?
 	if (end_position == (file_length - 1)) {
-		// Delete the end of the file (truncate to new_file_length)
+		// EOF
+		
+		// Truncate to new_file_length
 		ftruncate(file_descriptor, new_file_length);
 		
 		// Add a newline char
 		file_stream.seekp(new_file_length - 1, ios::beg);
 		file_stream.write("\n", 1);
-	} else {
-		// Before EOF
 		
-		for (long long int i = start_position; i < (new_file_length - 1); i = (i + amount_to_store)) {
-			
-			long long int copy_to_this_position = i;
-			
-			long long int copy_from_this_position = (i + remove_length);
-			
-			// Final iteration:
-			// If we discover that our block_size is more than all the data that's left,
-			// we just reset it to the amount of data that's left.
-			if ((copy_from_this_position + amount_to_store) > (file_length - 1)) {
-				amount_to_store = ((file_length - 1) - copy_from_this_position);
-			}
-			
-			char* temp_data_storage = new char[amount_to_store + 1]{0}; // Allocate memory
-			
-			// Store read portion into allocated memory
-			file_stream.seekg(copy_from_this_position, ios::beg);
-			file_stream.read(temp_data_storage, amount_to_store);
-			
-			// Add a NUL char to the end to terminate the string
-			temp_data_storage[amount_to_store] = 0;
-			
-			// Copy it to its new proper place
-			file_stream.seekp(copy_to_this_position, ios::beg);
-			file_stream.write(temp_data_storage, amount_to_store);
-			
-			delete[] temp_data_storage; // Free memory
+		return;
+	}
+	
+	// Before EOF
+	for (long long int i = start_position; i < (new_file_length - 1); i = (i + amount_to_store)) {
+		
+		long long int copy_to_this_position = i;
+		
+		long long int copy_from_this_position = (i + remove_length);
+		
+		// Final iteration:
+		// If we discover that our block_size is more than all the data that's left,
+		// we just reset it to the amount of data that's left.
+		if ((copy_from_this_position + amount_to_store) > (file_length - 1)) {
+			amount_to_store = ((file_length - 1) - copy_from_this_position);
 		}
 		
-		// Now, finally, truncate the file to its new length
-		ftruncate(file_descriptor, new_file_length);
+		char* temp_data_storage = new char[amount_to_store + 1]{0}; // Allocate memory
 		
-		// Add a newline char
-		file_stream.seekp(new_file_length - 1, ios::beg);
-		file_stream.write("\n", 1);
+		// Store read portion into allocated memory
+		file_stream.seekg(copy_from_this_position, ios::beg);
+		file_stream.read(temp_data_storage, amount_to_store);
+		
+		// Add a NUL char to the end to terminate the string
+		temp_data_storage[amount_to_store] = 0;
+		
+		// Copy it to its new proper place
+		file_stream.seekp(copy_to_this_position, ios::beg);
+		file_stream.write(temp_data_storage, amount_to_store);
+		
+		delete[] temp_data_storage; // Free memory
 	}
+	
+	// Now, finally, truncate the file to its new length
+	ftruncate(file_descriptor, new_file_length);
+	
+	// Add a newline char
+	file_stream.seekp(new_file_length - 1, ios::beg);
+	file_stream.write("\n", 1);
 }
 
 bool editor::file::execute_single_instruction(instruction instruction_to_execute) {
