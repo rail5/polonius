17ae5433217e57b712039eb8f110d9581e72af97
rail5 <andrew@rail5.org>
2023-04-05 17:00:38 -0400

    Clang has no support for fopen64

--- shared/definitions.h
+++ shared/definitions.h
@@ -8,3 +8,10 @@
 #define EXIT_BADOPT 3
 #define EXIT_OTHER 4
 #define EXIT_INTERRUPT 5
+
+#ifdef __clang__
+	#define fopen64 fopen
+	// clang doesn't have fopen64
+	// This might(?) mean that we can't open >2GB files on 32-bit systems which use clang/llvm
+	// Testing is needed
+#endif
