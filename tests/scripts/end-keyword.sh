407f34e32859a9ef030c218d41a7220820995be9
rail5 <andrew@rail5.org>
2024-09-05 19:57:46 +0800

    Improving the test procedures

--- tests/scripts/end-keyword.sh
+++ tests/scripts/end-keyword.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
-# Not written yet
+export all_ok=0
 
-echo "0"
+#shellcheck source=common
+. scripts/common
+
+# INSERT 'end'
+check_instruction "insert end this is a new file" "end-keywords" 1 1 1
+
+new_file=$(create_test_file "0123456789")
+check_instruction "insert end this is an old file" "end-keywords" 1 1 0 "$new_file"
+
+new_file=$(create_test_file "0123456789")
+check_output_of_instruction_is_correct "0123456789abc" "insert end abc" "end-keywords" 0 "$new_file"
+
+# REPLACE 'end'
+new_file=$(create_test_file "0123456789")
+check_output_of_instruction_is_correct "012345678a" "replace end a" "end-keywords" 0 "$new_file"
+
+new_file=$(create_test_file "0123456789")
+check_output_of_instruction_is_correct "9876543210" "replace end 9876543210" "end-keywords" 0 "$new_file"
+
+# REMOVE 'end'
+new_file=$(create_test_file "0123456789")
+check_output_of_instruction_is_correct "012345678" "remove end end" "end-keywords" 0 "$new_file"
+
+new_file=$(create_test_file "0123456789")
+check_output_of_instruction_is_correct "" "remove 0 end" "end-keywords" 0 "$new_file"
+
+new_file=$(create_test_file "0123456789")
+check_output_of_instruction_is_correct "01234" "remove 5 end" "end-keywords" 0 "$new_file"
+
+echo "$all_ok"
