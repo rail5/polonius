1ae266956fab3883690bbc6213e30333197810ad
rail5 <andrew@rail5.org>
2024-09-05 16:46:45 +0800

    Updated the 'are tests working' test

--- tests/scripts/000-are-tests-working.sh
+++ tests/scripts/000-are-tests-working.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Path is set to:" > debug/are-tests-working
-echo "$PATH" >> debug/are-tests-working
-
 if [[ "$(which polonius-editor)" == "$(realpath ../polonius-editor)" ]] && [[ "$(which polonius-reader)" == "$(realpath ../polonius-reader)" ]]; then
 	echo "0"
 	exit
 fi
 
 echo "1"
+# Output debug information to the relevant debug file if we failed
+echo "Path is set to:" > debug/are-tests-working
+echo "$PATH" >> debug/are-tests-working
