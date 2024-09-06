b99a0808e0bdd609703e3b1e4da6cd68e07cf326
rail5 <andrew@rail5.org>
2024-09-06 12:04:45 +0800

    Renamed write test

---
+++ tests/scripts/basic-write-file.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export all_ok=0
+
+#shellcheck source=common
+. scripts/common
+
+check_instruction "insert 0 hello world" "basic-write-file" 1 1 1
+
+check_output_of_instruction_is_correct "hello world" "insert 0 hello world" "basic-write-file" 1
+
+echo "$all_ok"
