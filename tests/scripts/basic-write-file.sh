cb059b1eecf808d28909a27bf544371566d5ac51
rail5 <andrew@rail5.org>
2024-09-06 12:26:39 +0800

    Added special char parsing to basic write tests

--- tests/scripts/basic-write-file.sh
+++ tests/scripts/basic-write-file.sh
@@ -12,4 +12,13 @@ check_output_of_instruction_is_correct "hello world" "insert 0 hello world" "bas
 new_file=$(create_test_file "0123456789")
 check_output_of_instruction_is_correct "012abc3456789" "insert 3 abc" "basic-write-file" 0 "$new_file"
 
+check_instruction "insert 0 \x01\x02\x03" "basic-write-file" 1 1 1
+
+check_output_of_instruction_is_correct "   " "insert 0 \x20\x20\x20" "basic-write-file" 1
+
+new_file=$(create_test_file "0123456789")
+check_output_of_instruction_is_correct "0123 456789" "insert 4 \x20" "basic-write-file" 0 "$new_file"
+
+check_output_of_instruction_is_correct "a b c" "insert 0 a\x20b\x20c" "basic-write-file" 1
+
 echo "$all_ok"
