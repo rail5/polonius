ac4989b0fa2a284c49f1ade2e573e295cc0ed62b
rail5 <andrew@rail5.org>
2024-09-06 12:07:05 +0800

    Updated write test

--- tests/scripts/basic-write-file.sh
+++ tests/scripts/basic-write-file.sh
@@ -9,4 +9,7 @@ check_instruction "insert 0 hello world" "basic-write-file" 1 1 1
 
 check_output_of_instruction_is_correct "hello world" "insert 0 hello world" "basic-write-file" 1
 
+new_file=$(create_test_file "0123456789")
+check_output_of_instruction_is_correct "012abc3456789" "insert 3 abc" "basic-write-file" 0 "$new_file"
+
 echo "$all_ok"
