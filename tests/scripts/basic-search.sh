a58aa98fcd33e29e7c18c5b634897c250a203ebc
rail5 <andrew@rail5.org>
2024-09-06 12:20:29 +0800

    Added some special chars parse attempts to the basic search test

--- tests/scripts/basic-search.sh
+++ tests/scripts/basic-search.sh
@@ -14,4 +14,13 @@ check_reader_command "basic-search" "$new_file" 0 1 "999"
 new_file="$(create_test_file "0123456789")"
 check_reader_command "basic-search" "$new_file" 0 1 "567" 0 0 0 0 3
 
+new_file="$(create_test_file "a b")"
+check_reader_command "basic-search" "$new_file" 1 1 "\x20" 0 1
+
+new_file="$(create_test_file "a b")"
+check_reader_output_is_correct " " "basic-search" "$new_file" 1 "\x20" 0 1
+
+new_file="$(create_test_file "a b")"
+check_reader_output_is_correct "1 1" "basic-search" "$new_file" 1 "\x20" 0 1 1
+
 echo "$all_ok"
