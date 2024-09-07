8c4e413b2527ef9c1b3da7f8d37eb627782f86cc
rail5 <andrew@rail5.org>
2024-09-07 10:16:14 +0800

    Renaming instruction sets to instruction sequences within the testing infrastructure

--- tests/scripts/basic-instruction-sequences.sh
+++ tests/scripts/basic-instruction-sequences.sh
@@ -37,7 +37,7 @@ new_file=$(create_test_file "0123456789")
 check_output_of_instruction_is_correct "d f3456789" "replace 0 abc; 0 d\x20f" "$test_title" 0 "$new_file"
 
 
-check_output_of_instruction_set_is_correct "hello world" "insert 0 these contents will be removed
+check_output_of_instruction_sequence_is_correct "hello world" "insert 0 these contents will be removed
 remove 3 end; 0 1; 0 end
 insert 0 o; 0 l; 0 l; 0 e; 0 h
 insert end  ; 6 watla
