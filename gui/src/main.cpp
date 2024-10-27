1c0804b8b3436dba879ebffd65ad363e00b69f90
NatnaelTaddese <natnaeltaddese3@gmail.com>
2024-10-27 15:11:20 +0800

    QT with makfile works and cross builds for windows and linux. Generic editor example as a proof of concept

---
+++ gui/src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by NAT on 10/27/2024.
+//
+
+#include "main.h"
---
+++ gui/src/main.h
@@ -0,0 +1,16 @@
+//
+// Created by NAT on 10/27/2024.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
