068494d90e28fc264d442ecf8764fe7f05e464a9
rail5 <andrew@rail5.org>
2025-07-07 15:42:09 +0800

    Removed old, unnecessary Instruction class (now replaced with Blocks and Expressions)

--- src/edit/editor.h
+++ src/edit/editor.h
@@ -23,36 +23,6 @@ namespace Editor {
 
 extern bool append_newline; // Whether to append a newline to the end of the file after executing instructions
 
-class Instruction;
-
-enum InstructionType : uint8_t {
-	INSERT,
-	REMOVE,
-	REPLACE
-};
-
-class Instruction {
-	private:
-		InstructionType op;
-		uint64_t start_position;
-		uint64_t end_position;
-		std::string text;
-	public:
-		Instruction() = default;
-		Instruction(InstructionType type, uint64_t start, const std::string& text);
-		Instruction(InstructionType type, uint64_t start, uint64_t end);
-		InstructionType type() const;
-		uint64_t start() const;
-		uint64_t end() const;
-		uint64_t size() const;
-		const std::string& get_text() const;
-
-		void set_type(InstructionType type);
-		void set_start(uint64_t start);
-		void set_end(uint64_t end);
-		void set_text(const std::string& text);
-};
-
 } // namespace Editor
 } // namespace Polonius
 
